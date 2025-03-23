fbf5a318ef0e8312a71fc838ce2436377311b818
Joe <akindelejoe@yahoo.com>
2025-03-23 12:01:11 -0500

    updated week-1

--- week-1/project08-01/project08-01_txt.js
+++ week-1/project08-01/project08-01_txt.js
@@ -11,14 +11,52 @@
 */
 
 /*--------------- Object Code --------------------*/
-
-
-
-
-
-
-
+let timer = {
+      minutes: 1,
+      seconds: 15,
+      running: false,
+      interval: null,
+   
+      start: function() {
+         if (!this.running) {
+            this.running = true;
+            this.interval = setInterval(() => {
+               if (this.seconds === 0 && this.minutes === 0) {
+                  this.stop();
+                  alert("Time's up!");
+               } else {
+                  if (this.seconds === 0) {
+                     this.minutes--;
+                     this.seconds = 59;
+                  } else {
+                     this.seconds--;
+                  }
+                  updateDisplay();
+               }
+            }, 1000);
+         }
+      },
+   
+      stop: function() {
+         this.running = false;
+         clearInterval(this.interval);
+      }
+   };
 /*---------------Interface Code -----------------*/
+function updateDisplay() {
+      minBox.value = timer.minutes;
+      secBox.value = timer.seconds;
+   }
+   
+   runPauseButton.addEventListener("click", function() {
+      if (timer.running) {
+         timer.stop();
+      } else {
+         timer.minutes = parseInt(minBox.value, 10);
+         timer.seconds = parseInt(secBox.value, 10);
+         timer.start();
+      }
+   });
 
 /* Interface Objects */
 let minBox = document.getElementById("minutesBox");
