83fed0815d9e4700e402f759b27f5c852a3718ad
Joe <akindelejoe@yahoo.com>
2025-03-29 17:48:09 -0500

    Added week two

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,23 +3,48 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author:Joe Akindele
+  Date: 29 March 2025
+  Filename:script.js
 */
 
 "use strict";
 
-function createCharacter(name, gender, characterClass) {
+
   // TODO: Implement this function
-}
+  function createCharacter(name, gender, characterClass) {
+    let character = {
+      name: name,
+      gender: gender,
+      characterClass: characterClass,
+      describe() {
+        return `Meet ${this.name}, a ${this.gender} ${this.characterClass} ready for adventure!`;
+      },
+    };
+    return character;
+  }
+  
+  document.getElementById("generateHero").addEventListener("click", function (e) {
+    e.preventDefault();
+  
+    // Get form values
+    let heroName = document.getElementById("heroName").value;
+    let heroGender = document.getElementById("heroGender").value;
+    let heroClass = document.getElementById("heroClass").value;
+  
+    console.log("Hero Name:", heroName);
+    console.log("Hero Gender:", heroGender);
+    console.log("Hero Class:", heroClass);
+  
 
-document.getElementById("generateHero").addEventListener("click", function(e) {
-  e.preventDefault();
-
-  // TODO: Get form values
-
-  // TODO: Create character
-
+// TODO: Create character
+  if (heroName && heroGender && heroClass) {
+    let hero = createCharacter(heroName, heroGender, heroClass);
+  
   // TODO: Display character information
+  document.getElementById("characterOutput").innerText = hero.describe();
+} else {
+  document.getElementById("characterOutput").innerText = "Hero not selected!";
+}
 });
+
